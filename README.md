12a48bbaa700d4a912faa2311de6e4520cc0f44f
Andy Ferguson <andy@teknostatik.org>
2021-05-30 21:57:22 +0100

    Removing snaps for now. Need to do some troubleshooting.

--- README.md
+++ README.md
@@ -2,4 +2,23 @@
 
 This is a fork of my [Ubuntu scripts](https://github.com/teknostatik/deploy_ubuntu).
 
-Very much a work in progress, and I wouldn't recommend running this (yet).
+Very much a work in progress, and I wouldn't recommend running this on a machine you care about.
+
+## Installation
+
+Before you run this script ensure that you have `sudo` installed and that your user is in the group. To dothis run the following as root (replacing `<your username>` with your username)
+
+    apt install -y sudo
+    sudo usermod -aG sudo <your username>
+
+You will also need some extra repositories enabled. My `/etc/apt/sources.list` looks like this:
+
+    deb http://deb.debian.org/debian bullseye main contrib non-free
+    deb http://security.debian.org/debian-security bullseye/updates main contrib non-free
+
+
+Then download the script and make it executable:
+
+    wget https://raw.githubusercontent.com/teknostatik/debian/master/deploy_debian.sh
+    chmod 755 deploy_debian.sh
+    ./deploy_debian.sh
--- deploy_debian.sh
+++ deploy_debian.sh
@@ -20,17 +20,10 @@ sudo apt -y upgrade
 
 # Install the i3 window manager and some basic utilities
 
-sudo apt install -y i3 i3blocks feh arandr git curl byobu synaptic xautolock shellcheck barrier kitty zathura remmina pcmanfm qutebrowser lxde featherpad ranger irssi zsh hexchat
+sudo apt install -y i3 i3blocks feh arandr git curl byobu synaptic lightdm xautolock shellcheck xinit barrier kitty zathura remmina pcmanfm qutebrowser featherpad ranger hexchat
 # Need to investigate automating ulauncher installation on Debian
 # sudo add-sudo apt-repository ppa:agornostal/ulauncher -y
 # sudo apt install -y ulauncher
-
-# Install some snaps
-
-sudo apt install -y snapd
-sudo snap install multipass --classic
-sudo snap install bpytop
-sudo snap install unixbench
 
 # Install everything needed for ProtonVPN and Tor
 # See https://protonvpn.com/support/linux-vpn-tool/ for how to install
@@ -42,6 +35,7 @@ sudo protonvpn init
 # Download and install a custom update script
 
 wget https://raw.githubusercontent.com/teknostatik/updateall/master/updateall
+sudo mv updateall /usr/local/bin/
 sudo chmod 755 /usr/local/bin/updateall
 
 # Install some packages to make remote shells more interesting and then add them to the profile for the logged in user
@@ -52,7 +46,6 @@ echo "echo; neofetch;echo" >> .profile
 
 # Install the applications I use for writing, editing and previewing text
 
-sudo snap install --classic atom
 sudo apt install -y pandoc texlive texlive-latex-extra abiword
 
 # Install some desktop applications for creating, editing and playing common media types
@@ -65,17 +58,6 @@ echo "alias ls='ls -la'" >> .bashrc
 echo "alias mp='multipass list'" >> .bashrc
 echo "alias top='bpytop'" >> .bashrc
 
-# Build a container running the latest Ubuntu LTS for testing things on and running any software that won't work on Debian
-# This will work on a machine with 8GB of RAM, despite what it looks like
-
-multipass launch -m 8G -d 20G lts --name ubuntu-lts
-multipass exec ubuntu-lts -- wget https://raw.githubusercontent.com/teknostatik/deploy_ubuntu/main/deploy_ubuntu_wsl.sh
-multipass exec ubuntu-lts -- sudo mv deploy_ubuntu_wsl.sh /usr/local/bin/
-multipass exec ubuntu-lts -- sudo chmod 755 /usr/local/bin/deploy_ubuntu_wsl.sh
-multipass exec ubuntu-lts -- deploy_ubuntu_wsl.sh
-multipass stop ubuntu-lts
-multipass set client.primary-name=ubuntu-lts
-
 # Download and install Dropbox
 
 sudo apt install -y nautilus-dropbox
@@ -84,21 +66,23 @@ dropbox start -i
 # Set up i3. Comment this out if you want to use your own config file or build your config from scratch.
 
 wget https://raw.githubusercontent.com/teknostatik/i3_config/main/config
+mkdir .config
+mkdir .config/i3/
 mv config ~/.config/i3/config
 
 # Set up i3 wallpaper
 
-mkdir /usr/share/wallpaper
+sudo mkdir /usr/share/wallpaper
 cd /usr/share/wallpaper
-wget https://www.dropbox.com/s/0yg8txbgw0ifqmg/9dy0gvxq7fl61.png
-wget https://www.dropbox.com/s/j9mmfedrc8r9zba/231-2311974_big.jpg
-wget https://www.dropbox.com/s/cljxhezhxuu3nce/background.png
-wget https://www.dropbox.com/s/f2rkmbv13c8t769/1920x1080-dark-linux.png
-wget https://www.dropbox.com/s/1i7g8u5h6whd5dv/3430638.png
-wget https://www.dropbox.com/s/bkae9ethe3jqbod/jyrji9bnp3171.jpg
+sudo wget https://www.dropbox.com/s/0yg8txbgw0ifqmg/9dy0gvxq7fl61.png
+sudo wget https://www.dropbox.com/s/j9mmfedrc8r9zba/231-2311974_big.jpg
+sudo wget https://www.dropbox.com/s/cljxhezhxuu3nce/background.png
+sudo wget https://www.dropbox.com/s/f2rkmbv13c8t769/1920x1080-dark-linux.png
+sudo wget https://www.dropbox.com/s/1i7g8u5h6whd5dv/3430638.png
+sudo wget https://www.dropbox.com/s/bkae9ethe3jqbod/jyrji9bnp3171.jpg
 cd $HOME
 wget https://raw.githubusercontent.com/teknostatik/i3_config/main/randomise_wallpaper
-mv randomise_wallpaper /usr/local/bin/
-chmod 755 /usr/local/bin/randomise_wallpaper
+sudo mv randomise_wallpaper /usr/local/bin/
+sudo chmod 755 /usr/local/bin/randomise_wallpaper
 
 echo "The script has now finished running."
