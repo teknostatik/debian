5b93c5c5b4bdee1b2f46284a86f93973b5d6e659
Andy Ferguson <andy@teknostatik.org>
2021-07-26 08:50:48 +0100

    Referencing new i3 installation script

--- deploy_debian.sh
+++ deploy_debian.sh
@@ -18,9 +18,15 @@ set -euo pipefail
 sudo apt update
 sudo apt -y upgrade
 
-# Install the i3 window manager and some basic utilities
+# Install some basic utilities
 
-sudo apt install -y i3 i3blocks feh htop arandr git byobu synaptic lightdm xautolock shellcheck xinit kitty zathura pcmanfm featherpad firefox-esr
+sudo apt install -y htop git byobu synaptic lightdm xautolock shellcheck xinit kitty zathura pcmanfm featherpad firefox-esr
+
+# Install and configure i3
+
+wget https://raw.githubusercontent.com/teknostatik/i3_config/main/install_i3.sh
+sudo mv install_i3.sh /usr/local/bin/
+sudo chmod 755 /usr/local/bin/install_i3.sh
 
 # Install everything needed for Tor
 # See https://protonvpn.com/support/linux-vpn-tool/ for how to install
@@ -56,31 +62,6 @@ echo "alias top='htop'" >> .bashrc
 
 sudo apt install -y nautilus-dropbox
 dropbox start -i
-
-# Set up i3. Comment this out if you want to use your own config file or build your config from scratch.
-
-wget https://raw.githubusercontent.com/teknostatik/i3_config/main/config
-mkdir .config
-mkdir .config/i3/
-mv config ~/.config/i3/config
-
-# Set up i3 wallpaper
-
-udo mkdir /usr/share/wallpaper
-cd /usr/share/wallpaper
-sudo wget https://www.dropbox.com/s/0yg8txbgw0ifqmg/9dy0gvxq7fl61.png
-sudo wget https://www.dropbox.com/s/cljxhezhxuu3nce/background.png
-sudo wget https://www.dropbox.com/s/f2rkmbv13c8t769/1920x1080-dark-linux.png
-sudo wget https://www.dropbox.com/s/1i7g8u5h6whd5dv/3430638.png
-sudo wget https://www.dropbox.com/s/bkae9ethe3jqbod/jyrji9bnp3171.jpg
-sudo wget https://www.dropbox.com/s/idk05cia43lj5qb/rocket.png
-cd $HOME
-wget https://raw.githubusercontent.com/teknostatik/i3_config/main/randomise_wallpaper
-sudo mv randomise_wallpaper /usr/local/bin/
-sudo chmod 755 /usr/local/bin/randomise_wallpaper
-wget https://raw.githubusercontent.com/teknostatik/i3_config/main/lock.sh
-sudo mv lock.sh /usr/local/bin/
-sudo chmod 755 /usr/local/bin/lock.sh
 
 # Install some drivers that my hardware requires (mostly for wifi)
 
