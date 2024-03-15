32340eb49abf2a60d099e3eb26c31db1ac1e79ce
Andy Ferguson <andy@teknostatik.org>
2024-03-15 21:38:44 +0000

    A better way to install VSCode

--- deploy_debian.sh
+++ deploy_debian.sh
@@ -80,6 +80,14 @@ echo "echo; fastfetch;echo" >> .profile
 # Install the applications I use for writing, editing and previewing text
 
 sudo apt install -y pandoc texlive texlive-latex-extra
+sudo apt-get install gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
 
 # Install some desktop applications for creating, editing and playing common media types
 
