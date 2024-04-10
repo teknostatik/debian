5634dcdd53c9a7c42492b39644327e869887105f
Andy Ferguson <andy@teknostatik.org>
2024-04-10 19:56:50 +0100

    Bugfixes following testing

--- deploy_debian.sh
+++ deploy_debian.sh
@@ -60,10 +60,6 @@ wget https://raw.githubusercontent.com/teknostatik/i3_config/main/randomise_wall
 sudo mv randomise_wallpaper /usr/local/bin/
 sudo chmod 755 /usr/local/bin/randomise_wallpaper
 
-# Install everything needed for Tor
-
-sudo apt install -y torbrowser-launcher onionshare
-
 # Download and install a custom update script
 
 wget https://raw.githubusercontent.com/teknostatik/updateall/master/updateall
@@ -80,19 +76,19 @@ echo "echo; fastfetch;echo" >> .profile
 
 # Install the applications I use for writing, editing and previewing text
 
-sudo apt install -y pandoc texlive texlive-latex-extra abiword
+sudo apt install -y pandoc texlive texlive-latex-extra
 sudo apt-get install gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
+sudo apt install -y apt-transport-https
 sudo apt update
-sudo apt install code
+sudo apt install -y code
 
 # Install some desktop applications for creating, editing and playing common media types
 
-sudo apt install -y gimp rhythmbox vlc brasero sound-juicer transmission
+sudo apt install -y gimp rhythmbox vlc brasero sound-juicer
 
 # Install Flatpak
 
@@ -143,7 +139,7 @@ sudo apt install -y proton-vpn-gnome-desktop
 
 # Install Zerotier
 
-curl -s https://install.zerotier.com | sudo bash
+# curl -s https://install.zerotier.com | sudo bash
 
 # Download unixbench
 
@@ -158,4 +154,4 @@ git clone https://github.com/kdlucas/byte-unixbench.git
 sudo apt install -y nautilus-dropbox
 dropbox start -i
 
-echo "The script has now finished running.
+echo "The script has now finished running."
