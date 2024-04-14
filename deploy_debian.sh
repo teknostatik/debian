035cc2e311de50ccff7c5a381b79b2cf6a91e23d
Andy Ferguson <andy@teknostatik.org>
2024-04-14 19:57:44 +0100

    added blueman

--- deploy_debian.sh
+++ deploy_debian.sh
@@ -29,7 +29,7 @@ sudo apt install -y htop git byobu synaptic xautolock shellcheck xinit kitty zat
 
 # Install the i3 window manager and some basic utilities (all of these are referenced in my i3 config file, so need to be installed)
 
-sudo apt install -y i3 i3blocks feh arandr scrot xautolock barrier kitty imagemagick polybar caffeine copyq picom
+sudo apt install -y i3 i3blocks feh arandr scrot xautolock barrier kitty imagemagick polybar caffeine copyq picom blueman
 
 # Set up i3. Comment this out if you want to use your own config file or build your config from scratch.
 
