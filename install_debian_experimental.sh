7e05953d9683840f35fc39e6ee78994ad1e5e4ec
Andy Ferguson <andy@teknostatik.org>
2024-08-06 15:58:18 +0100

    Added last few packages and optimised code

--- install_debian_experimental.sh
+++ install_debian_experimental.sh
@@ -45,54 +45,49 @@ sudo apt install -y fortune-mod cowsay
 echo "echo; fortune | cowsay;echo" >> .profile
 echo "echo; fastfetch;echo" >> .profile
 
+# Some optional packages, which users can choose to install
+
 # Function to install vscode
 install_vscode() {
-    sudo apt-get install gpg
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-    rm -f packages.microsoft.gpg
-    sudo apt install -y apt-transport-https
-    sudo apt update
-    sudo apt install -y code
+    sudo apt-get install -y gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/keyrings/packages.microsoft.gpg > /dev/null
+    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    sudo apt-get install -y apt-transport-https
+    sudo apt-get update
+    sudo apt-get install -y code
 }
 
 # Function to install and configure i3
 install_i3() {
-    sudo apt install -y i3 i3blocks feh arandr scrot xautolock barrier kitty imagemagick polybar caffeine copyq picom blueman pcmanfm 
-    wget https://raw.githubusercontent.com/teknostatik/i3_config/main/config
-    wget https://raw.githubusercontent.com/teknostatik/i3_config/main/lock.sh
-    wget https://raw.githubusercontent.com/teknostatik/i3_config/main/kitty.conf
-    wget https://raw.githubusercontent.com/teknostatik/i3_config/main/picom.conf
-    mkdir /home/$WHOAMI/.config/i3
-    mv config /home/$WHOAMI/.config/i3/
-    sudo mv lock.sh /usr/local/bin/
-    mkdir /home/$WHOAMI/.config/kitty
-    mv kitty.conf /home/$WHOAMI/.config/kitty/
-    mkdir /home/$WHOAMI/.config/picom
-    mv picom.conf /home/$WHOAMI/.config/picom/
-    sudo mkdir /usr/share/wallpaper
+    sudo apt-get install -y i3 i3blocks feh arandr scrot xautolock barrier kitty imagemagick polybar caffeine copyq picom blueman pcmanfm
+    wget -q https://raw.githubusercontent.com/teknostatik/i3_config/main/config -O /tmp/i3_config
+    wget -q https://raw.githubusercontent.com/teknostatik/i3_config/main/lock.sh -O /usr/local/bin/lock.sh
+    wget -q https://raw.githubusercontent.com/teknostatik/i3_config/main/kitty.conf -O /tmp/kitty.conf
+    wget -q https://raw.githubusercontent.com/teknostatik/i3_config/main/picom.conf -O /tmp/picom.conf
+    mkdir -p /home/$USER/.config/i3 /home/$USER/.config/kitty /home/$USER/.config/picom
+    mv /tmp/i3_config /home/$USER/.config/i3/config
+    mv /tmp/kitty.conf /home/$USER/.config/kitty/kitty.conf
+    mv /tmp/picom.conf /home/$USER/.config/picom/picom.conf
+    sudo install -D -o root -g root -m 755 /usr/local/bin/lock.sh /usr/local/bin/lock.sh
+    sudo mkdir -p /usr/share/wallpaper
     sudo cp -R /usr/share/backgrounds/* /usr/share/wallpaper
-    cd $HOME
-    wget https://raw.githubusercontent.com/teknostatik/i3_config/main/randomise_wallpaper
-    sudo mv randomise_wallpaper /usr/local/bin/
+    wget -q https://raw.githubusercontent.com/teknostatik/i3_config/main/randomise_wallpaper -O /usr/local/bin/randomise_wallpaper
     sudo chmod 755 /usr/local/bin/randomise_wallpaper
 }
 
 # Function to install tor
 install_tor() {
-    sudo apt install -y torbrowser-launcher onionshare
+    sudo apt-get install -y torbrowser-launcher onionshare
 }
 
 # Function to install DisplayLink
 install_displaylink() {
-    git clone https://github.com/AdnanHodzic/displaylink-debian.git
-    cd displaylink-debian
-    sudo ./displaylink-debian.sh
-    wget https://raw.githubusercontent.com/teknostatik/debian/master/20-displaylink.conf
-    sudo mv 20-displaylink.conf /etc/X11/xorg.conf.d/
-    cd ..
-} 
+    git clone https://github.com/AdnanHodzic/displaylink-debian.git /tmp/displaylink-debian
+    echo "Do not reboot when given the option. You will need to reboot before trying to use your docking station."
+    sudo /tmp/displaylink-debian/displaylink-debian.sh
+    wget -q https://raw.githubusercontent.com/teknostatik/debian/master/20-displaylink.conf -O /etc/X11/xorg.conf.d/20-displaylink.conf
+    rm -rf /tmp/displaylink-debian
+}
 
 # Function to install snapd
 install_snapd() {
@@ -107,60 +102,62 @@ install_flatpak() {
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 }
 
-# Prompt the user for VScode installation
-echo "Do you want to install Visual Studio Code? (yes/no)"
-read vscode_choice
+# Function to install ProtonVPN
+install_protonvpn() {
+    wget -q https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb -O /tmp/protonvpn.deb
+    sudo dpkg -i /tmp/protonvpn.deb
+    sudo apt-get update
+    sudo apt-get install -y proton-vpn-gnome-desktop
+    rm /tmp/protonvpn.deb
+}
 
-if [[ "$vscode_choice" == "yes" ]]; then
-    install_vscode
-fi
+# Function to install Zerotier 
+install_zerotier() {
+    curl -s https://install.zerotier.com | sudo bash
+}
 
-# Prompt the user for i3 installation
-echo "Do you want to install and configure the i3 tiling window manager? (yes/no)"
-read i3_choice
+# Function to install Unixbench
+install_unixbench() {
+    sudo apt-get install libx11-dev libgl1-mesa-dev libxext-dev perl perl-modules make git
+    git clone https://github.com/kdlucas/byte-unixbench.git
+# uncomment tne next 2 lines to run the benchmark now
+# cd byte-unixbench/UnixBench/
+# ./Run
+}
 
-if [[ "$i3_choice" == "yes" ]]; then
-    install_i3
-fi
+# Fuction to install Dropbox
+install_dropbox() {
+    sudo apt install -y nautilus-dropbox
+    dropbox start -i
+}
 
-# Prompt the user for tor installation
-echo "Do you want to install and configure Tor browser and Onionshare? (yes/no)"
-read tor_choice
+# Prompt function
+prompt_install() {
+    read -p "Do you want to install $1? (yes/no): " choice
+    if [[ "$choice" == "yes" ]]; then
+        $2
+    fi
+}
 
-if [[ "$tor_choice" == "yes" ]]; then
-    install_tor
-fi
-
-# Prompt the user for DisplayLink installation
-echo "Will you be using a DisplayLink docking station on this computer? (yes/no)"
-read displaylink_choice
-
-if [[ "$displaylink_choice" == "yes" ]]; then
-    install_displaylink
-fi
-
-# Prompt the user for snapd installation
-echo "Do you want to install snapd? (yes/no)"
-read snapd_choice
-
-if [[ "$snapd_choice" == "yes" ]]; then
-    install_snapd
-fi
-
-# Prompt the user for flatpak installation
-echo "Do you want to install flatpak? (yes/no)"
-read flatpak_choice
-
-if [[ "$flatpak_choice" == "yes" ]]; then
-    install_flatpak
-fi
+# Main script to prompt user and call installation functions
+prompt_install "Visual Studio Code" install_vscode
+prompt_install "i3 tiling window manager" install_i3
+prompt_install "Tor browser and Onionshare" install_tor
+prompt_install "DisplayLink docking station support" install_displaylink
+prompt_install "snapd" install_snapd
+prompt_install "flatpak" install_flatpak
+prompt_install "ProtonVPN" install_protonvpn
+prompt_install "Zerotier" install_zerotier
+prompt_install "Unixbench" install_unixbench
+prompt_install "Dropbox" install_dropbox
 
 # Add some aliases
 
 echo "alias ls='ls -la'" >> .bashrc
 echo "alias top='htop'" >> .bashrc
 
-# Set up git
+# Set up git 
+# This only works if you are me, which I'll fix at some point
 
 git config --global user.name "Andy Ferguson"
 git config --global user.email "andy@teknostatik.org"
